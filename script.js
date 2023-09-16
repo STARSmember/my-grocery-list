d8d6e1e6a7438d3b3a100145778a0d933921484e
Julian Fragoso <starsmember@yahoo.com>
2023-09-15 23:48:00 -0500

    Added local storage

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 
+// import PeaDB provided by the Video. the skypack does not load PeaDB page.
+import {LocalDB} from 'http://cdn.skypack.dev/peadb'
+
+// import Short ID package. It does not show up on website
+import shortid from 'https://cdn.skypack.dev/shortid'
+
+// link both the "import peadb" with 'const groceries' array
+// by creating a const.
+   const db = new LocalDB('grocery-list-db')   // const is linked to groceries array <----- IMPORTANT
+
+
 // create an array to hold the list of items you have created. 
 // here is the array show at the bottom...
 // const groceries = ['Milk', 'Cookie', 'Orange Juice']   // <---- array created. <---- OLD array
 // array above this line of code.
 
 // create a const array like the one above but CONST.
-
-const groceries = [] // <-- new Array.
+// const groceries = [] // <-- new Array.
+   const groceries = db.getAll() || [] // <--- NEW, new array
 ///////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////
 
@@ -21,7 +32,7 @@ const addBtn = document.getElementById('addBtn')
 
 const createGroceryElement = grocery => {
     const groceryElement = document.createElement('li')
-    groceryElement.innerText = grocery
+    groceryElement.innerText = grocery.value
     // add "classList.add" DONT not forget the 'dot' between List AMD add.
     groceryElement.classList.add('groceryItem')
     return groceryElement
@@ -34,8 +45,11 @@ const addGrocery = newGrocery => {
 addBtn.addEventListener('click', e => {
     e.preventDefault()
     const value = newGroceryInput.value
+    
     if (value) {
-        addGrocery(value)
+        const key =shortid.generate()
+        addGrocery({ key,value })  // <---- important part. create the ARRAY using ---> brackets {}
+        db.set(key, value) 
         newGroceryInput.value = null
     }
 
